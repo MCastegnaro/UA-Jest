f93ace5583043f9fcd928045de9a0628e908e51b
n3xtizy <martinsgabriel0312@gmail.com>
2023-04-11 21:15:54 -0300

    envio

---
+++ src/Conta.test.js
@@ -0,0 +1,56 @@
+const Conta = require("./Conta");
+
+describe("Conta", () =>{
+    test("Deve depositar com conta ativa", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.0);
+
+        expect(conta.getSaldo()).toBe(100);
+        
+    });
+
+    test("Não Deve depositar com conta inativa", () => {
+        const conta = new Conta();
+
+        conta.inativar();
+        conta.depositar(100.0);
+
+        expect(conta.getSaldo()).toBe(0);       
+    });
+
+    test("Deve sacar com conta ativa e saldo maior que o valor de saque", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.0);
+        conta.sacar(70.0);
+
+        expect(conta.getSaldo()).toBe(30);
+        
+    });
+
+    test("Não deve sacar com conta inativa", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.0);
+        conta.inativar();
+        conta.sacar(70.0);
+
+        expect(conta.getSaldo()).toBe(100);
+        
+    });
+
+    test("Não deve sacar com conta ativa e saldo menor que o valor de saque", () => {
+        const conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100.0);
+        conta.sacar(110.0);
+
+        expect(conta.getSaldo()).toBe(100);
+        
+    });
+})
